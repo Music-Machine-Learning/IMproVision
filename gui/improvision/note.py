743ea5b2ccd1adee9014390345419c7e5c3459da
Marco Melletti <marco.melletti@sadel.it>
2021-08-27 12:49:37 +0200

    note: removed noteon parameter from note class, embedded bending information in note __str__ and from_string methods, fixed freq method to actually display note frequency with bending

--- gui/improvision/note.py
+++ gui/improvision/note.py
@@ -1,45 +1,40 @@
 import math
 
+
 class Note:
     _note_names = [("C"), ("C#", "Db"), ("D"), ("D#", "Eb"), ("E"), ("F"), ("F#", "Gb"), ("G"), ("G#", "Ab"), ("A"), ("A#", "Bb"), ("B")]
     _base_octave = -1
 
-    def __init__(self, note, bend=0, noteon=True):
+    def __init__(self, note, bend=0):
         self.note = int(note)
         if note < 0 or note > 127:
             raise AttributeError("note {} out of midi range".format(note))
         self.bend = int(bend)
         if bend < 0 or bend > 127:
             raise AttributeError("bend {} out of midi range".format(bend))
-        self.noteon = noteon
+
+    def __repr__(self):
+        return str(self) + ", note: {}, bend: {}, freq: {}".format(self.note, self.bend, self.freq())
+
+    def __hash__(self):
+        return hash((self.note, self.bend))
+
+    def __eq__(self, other):
+        return self.note == other.note and self.bend == other.bend
+
+    def __lt__(self, other):
+        return self.note < other.note or (self.note == other.note and self.bend < other.bend)
 
     def __str__(self):
         note = self._note_names[self.note % len(self._note_names)][0]
         octave = int(math.floor(self.note / len(self._note_names)) + self._base_octave)
-        return note + str(octave)
-
-    def __repr__(self):
-        return str(self) + " ({}, f: {}), b: {}, on: {}".format(self.note, self.freq(), self.bend, self.noteon)
-
-    def __hash__(self):
-        return hash((self.note, self.bend, self.noteon))
-
-    def __eq__(self, other):
-        return self.note == other.note and self.bend == other.bend and self.noteon == other.noteon
-
-    def __lt__(self, other):
-        return self.note < other.note or (
-                self.note == other.note and self.bend < other.bend or
-                (
-                        self.note == other.note and self.bend == other.bend and not self.noteon and other.noteon
-                )
-        )
-
-    def freq(self):
-        return 440 * 2 ** ((self.note - 69) / 12)
+        if self.bend == 0:
+            return note + str(octave)
+        else:
+            return note + str(octave) + "+" + str(self.bend)
 
     @staticmethod
-    def from_string(notename: str, noteon=True):
+    def from_string(notename: str):
         if type(notename) is not str:
             raise AttributeError("{} must be a string".format(notename))
         notename = notename.strip()
@@ -54,20 +49,38 @@ class Note:
         if notenum is None:
             raise AttributeError("{} is not a valid note name".format(notename))
 
-        octave = notename[len(note):]
+        notename = notename[len(note):]
+
+        if '+' in notename:
+            octave = notename[:notename.index('+')]
+            bend = notename[notename.index('+')+1:]
+        else:
+            octave = notename
+            bend = "0"
+
         try:
             octavenum = int(octave) - Note._base_octave
         except ValueError:
             raise AttributeError("{} is not a valid octave number ({})".format(octave, notename))
 
-        return Note(notenum + (octavenum * 12), noteon=noteon)
+        try:
+            bendnum = int(bend)
+        except ValueError:
+            raise AttributeError("{} is not a valid bending value ({})".format(bend, notename))
+
+        return Note(notenum + (octavenum * 12), bendnum)
+
+    def freq(self):
+        notefreq = 440 * 2 ** ((self.note - 69) / 12)
+        return notefreq * 2 ** (self.bend / (12 * 128))
 
     @staticmethod
-    def from_frequency(freq: float, noteon=True):
+    def from_frequency(freq: float):
         note = int(12 * math.log2(freq/440) + 69)
         bend = int((12 * 128) * math.log2(freq / Note(note).freq()))
         if bend < 0:
             note -= 1
             bend = 127 + bend
-        return Note(note, bend, noteon)
+        return Note(note, bend)
+
 
