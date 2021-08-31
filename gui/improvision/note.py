80eb967ec3dc6fa7bf2c20e120817730f2796e18
Marco Melletti <marco.melletti@sadel.it>
2021-08-31 19:58:13 +0200

    added copyright notice

--- gui/improvision/note.py
+++ gui/improvision/note.py
@@ -1,3 +1,12 @@
+# coding=utf-8
+# Copyright (C) 2021 by Marco Melletti <mellotanica@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+
 import math
 from .configurable import Configuration
 
