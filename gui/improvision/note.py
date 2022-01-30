e124e8f64cc4e3f300a42cd4de5f4e76554339df
Marco Melletti <mellotanica@hotmail.it>
2022-01-30 15:10:22 +0100

    note: added velocity concept to notes in both inner and textual representation

--- gui/improvision/note.py
+++ gui/improvision/note.py
@@ -14,18 +14,32 @@ from lib.gibindings import Gtk
 
 
 class Note:
-    _note_names = [("C"), ("C#", "Db"), ("D"), ("D#", "Eb"), ("E"), ("F"), ("F#", "Gb"), ("G"), ("G#", "Ab"), ("A"), ("A#", "Bb"), ("B")]
+    _note_names = [
+        ("C"),
+        ("C#", "Db"),
+        ("D"),
+        ("D#", "Eb"),
+        ("E"),
+        ("F"),
+        ("F#", "Gb"),
+        ("G"),
+        ("G#", "Ab"),
+        ("A"),
+        ("A#", "Bb"),
+        ("B"),
+    ]
     _base_octave = -1
 
-    def __init__(self, notedef):
-        '''
+    def __init__(self, notedef, velocity=127):
+        """
         :param notedef: based on the type the parameter will be interpreted as:
            - string: decode note from string name (e.g. A2, C#4+24, ...)
            - float: create note from frequency
            - int: create a note without any bending and specified note number
            - tuple: explicitly set note number and bend (e.g. (69, 0), (12, 32), ...)
            - Note: copy other note's values
-        '''
+        :param velocity: note intensity (accepted range [0-127])
+        """
 
         self.bend = 0
         if type(notedef) is int:
@@ -41,25 +55,43 @@ class Note:
             self.note = notedef.note
             self.bend = notedef.bend
 
-        if self.note < 0 or self.note > 127:
-            raise AttributeError("note {} out of midi range".format(self.note))
-        if self.bend < 0 or self.bend > 127:
-            raise AttributeError("bend {} out of midi range".format(self.bend))
+        self.velocity = velocity
+        self.__validate__()
 
     def __repr__(self):
-        return str(self) + ", note: {}, bend: {}, freq: {}".format(self.note, self.bend, self.freq())
+        return str(self) + ", note: {}, bend: {}, freq: {}, velo: {}".format(
+            self.note, self.bend, self.freq(), self.velocity
+        )
 
     def __bytes__(self):
         return bytes(str(self))
 
     def __hash__(self):
-        return hash((self.note, self.bend))
+        return hash((self.note, self.bend, self.velocity))
 
     def __eq__(self, other):
-        return self.note == other.note and self.bend == other.bend
+        return (
+            self.note == other.note
+            and self.bend == other.bend
+            and self.velocity == other.velocity
+        )
 
     def __lt__(self, other):
-        return self.note < other.note or (self.note == other.note and self.bend < other.bend)
+        return self.note < other.note or (
+            self.note == other.note
+            and (
+                self.bend < other.bend
+                or (self.bend == other.bend and self.velocity < other.velocity)
+            )
+        )
+
+    def __validate__(self):
+        if self.note < 0 or self.note > 127:
+            raise AttributeError("note {} out of midi range".format(self.note))
+        if self.bend < 0 or self.bend > 127:
+            raise AttributeError("bend {} out of midi range".format(self.bend))
+        if self.velocity < 0 or self.velocity > 127:
+            raise AttributeError("velocity {} out of midi range".format(self.velocity))
 
     def __add__(self, other):
         on = Note(other)
@@ -87,11 +119,14 @@ class Note:
 
     def __str__(self):
         note = self._note_names[self.note % len(self._note_names)][0]
-        octave = int(math.floor(self.note / len(self._note_names)) + self._base_octave)
-        if self.bend == 0:
-            return note + str(octave)
-        else:
-            return note + str(octave) + "+" + str(self.bend)
+        note += str(
+            int(math.floor(self.note / len(self._note_names)) + self._base_octave)
+        )
+        if self.bend != 0:
+            note += f"+{self.bend}"
+        if self.velocity < 127:
+            note += f"%{self.velocity}"
+        return note
 
     def __from_string(self, notename: str):
         if type(notename) is not str:
@@ -99,7 +134,7 @@ class Note:
         notename = notename.strip()
         notename = notename[0].upper() + notename[1:].lower()
         note = notename[:1]
-        if notename[1] == '#' or notename[1] == 'b':
+        if notename[1] == "#" or notename[1] == "b":
             note = notename[:2]
         notenum = None
         for n in range(len(Note._note_names)):
@@ -108,11 +143,22 @@ class Note:
         if notenum is None:
             raise AttributeError("{} is not a valid note name".format(notename))
 
-        notename = notename[len(note):]
+        notename = notename[len(note) :]
 
-        if '+' in notename:
-            octave = notename[:notename.index('+')]
-            bend = notename[notename.index('+')+1:]
+        if "%" in notename:
+            try:
+                self.velocity = int(notename[notename.index("%") + 1 :])
+            except ValueError:
+                raise AttributeError(
+                    "{} is not a valid velocity value ({})".format(velocity, notename)
+                )
+            notename = notename[: notename.index("%")]
+        else:
+            self.velocity = 127
+
+        if "+" in notename:
+            octave = notename[: notename.index("+")]
+            bend = notename[notename.index("+") + 1 :]
         else:
             octave = notename
             bend = "0"
@@ -120,28 +166,44 @@ class Note:
         try:
             octavenum = int(octave) - Note._base_octave
         except ValueError:
-            raise AttributeError("{} is not a valid octave number ({})".format(octave, notename))
+            raise AttributeError(
+                "{} is not a valid octave number ({})".format(octave, notename)
+            )
         self.note = notenum + (octavenum * 12)
 
         try:
             self.bend = int(bend)
         except ValueError:
-            raise AttributeError("{} is not a valid bending value ({})".format(bend, notename))
+            raise AttributeError(
+                "{} is not a valid bending value ({})".format(bend, notename)
+            )
+
+        self.__validate__()
 
     def freq(self):
         notefreq = 440 * 2 ** ((self.note - 69) / 12)
         return notefreq * 2 ** (self.bend / (12 * 128))
 
     def __from_frequency(self, freq: float):
-        self.note = int(12 * math.log2(freq/440) + 69)
+        self.note = int(12 * math.log2(freq / 440) + 69)
         self.bend = int((12 * 128) * math.log2(freq / self.freq()))
         if self.bend < 0:
             self.note -= 1
             self.bend = 127 + self.bend
 
+        self.__validate__()
+
 
 class NoteConfiguration(Configuration):
-    def __init__(self, name: str, pref_path: str, dfl_val: Note, lower: Note = Note(0), upper: Note = Note((127, 127)), gui_setup_cb=None):
+    def __init__(
+        self,
+        name: str,
+        pref_path: str,
+        dfl_val: Note,
+        lower: Note = Note(0),
+        upper: Note = Note((127, 127)),
+        gui_setup_cb=None,
+    ):
         super().__init__(name, pref_path, str(dfl_val), gui_setup_cb)
         self._lower = lower
         self._upper = upper
@@ -151,10 +213,10 @@ class NoteConfiguration(Configuration):
         self._buf = Gtk.EntryBuffer()
         self._buf.set_text(value, len(value))
 
-        def _text_deleted(b,p,n):
+        def _text_deleted(b, p, n):
             self._set_value(str(self.get_value()))
 
-        def _text_inserted(b,p,c,n):
+        def _text_inserted(b, p, c, n):
             self._set_value(str(self.get_value()))
 
         self._buf.connect("deleted-text", _text_deleted)
